1892047ecedad9a40df6087f97c0efd6d6d649af
Cognosonline <soporte@cognosonline.com>
2024-05-21 17:44:19 -0500

    1

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express on Lambda!');
+    res.send('Hello from Express on Lambda prueba!');
 });
 
 app.listen(3000, () =>{
